e1bedb6925590e1667165314036551eea411fb46
Wald13 <waldmendes@live.com>
2025-09-03 20:07:39 +0100

    Update the message function

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Table, Booking
 from datetime import datetime, timedelta
